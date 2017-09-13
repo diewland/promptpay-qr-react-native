eef6a24047e7022f45fa63b40165aa0972bd0e61
Yuwat Chuesathuchon <yuwat@mfec.co.th>
2017-09-13 17:09:24 +0700

    keep input state

--- app/App.js
+++ app/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react'
 import QRCode from 'react-native-qrcode'
-import { StyleSheet, View, TextInput } from 'react-native'
+import {
+    StyleSheet,
+    View,
+    TextInput,
+    AsyncStorage
+} from 'react-native'
 import PromptPayQR from './promptpay-qr'
+
+// storage key
+const PPQR_DATA = "PPQR_DATA"
 
 class App extends Component {
 
+    // app data
     state = {
         text:   null,
         acc_id: '',
         amount: '',
     }
 
+    // component mount event
+    componentDidMount() {
+        AsyncStorage.getItem(PPQR_DATA).then((v) => {
+            if(v){
+                this.state = JSON.parse(v)
+                this.setState(this.state)
+            }
+        })
+    }
+
+    // app functions
     set_acc_id(txt){
         this.state.acc_id = txt;
         this.render_qr();
@@ -22,18 +42,22 @@ class App extends Component {
     render_qr(){
         let text = PromptPayQR.gen_text(this.state.acc_id, this.state.amount);
         this.setState({ text })
+        AsyncStorage.setItem(PPQR_DATA, JSON.stringify(this.state));
     }
 
+    // core render function
     render() {
         return (
             <View style={styles.container}>
                 <TextInput
+                    value={this.state.acc_id}
                     style={styles.input}
                     keyboardType="numeric"
                     placeholder="เบอร์โทรศัพท์ หรือ เลขบัตรปชช"
                     onChangeText={ (text) => this.set_acc_id(text) }
                 />
                 <TextInput
+                    value={this.state.amount}
                     style={styles.input}
                     keyboardType="numeric"
                     placeholder="จำนวนเงิน (ไม่ระบุก็ได้)"
